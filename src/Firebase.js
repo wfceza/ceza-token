a50ea9ba7a11b0f78189650ee7842f6afef9e1b9
wfceza <wfceza@gmail.com>
2024-05-29 12:01:22 -0700

    commit

--- src/Firebase.js
+++ src/Firebase.js
@@ -17,3 +17,7 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app)
+export const provider = new GoogleAuthProvider();
+export const db = getFirestore(app);
